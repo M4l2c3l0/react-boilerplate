c4793674bb348ff8239d76033c7c64ed29b65e27
Marcelo <marcelo@mission-communications.net>
2018-11-20 17:00:03 +0000

    Init commit

---
+++ src/app.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import AppRouter, { history } from './routers/AppRouter';
+import StoreConfig from './store/store-config';
+import { login, logout } from './actions/auth';
+import 'normalize.css/normalize.css';
+import './styles/app.scss';
+import 'react-dates/lib/css/_datepicker.css';
+import { firebase } from './database/firebase';
+import Loading from './components/Loading';
+
+let hasRendered = false;
+
+const store = StoreConfig();
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
+const renderApp = () => {
+  if(!hasRendered) {
+    ReactDOM.render(jsx, document.getElementById('app'));
+    hasRendered = true;
+  }
+};
+
+ReactDOM.render(<Loading />, document.getElementById('app'));
+
+firebase.auth().onAuthStateChanged((user) => {
+  if(user) {
+    store.dispatch(login(user.uid));
+    renderApp();
+    if(history.location.pathname === '/') {
+      history.push('/dashboard');
+    }
+  } else {
+    store.dispatch(logout());
+    renderApp();
+    history.push('/');
+  }
+});
